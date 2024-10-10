346c6feb729c466fe1c51f10158443a9f925f458
CarlosDev2207 <mendozasanchez.carloseduardo07@gmail.com>
2024-10-09 20:43:37 -0600

    clase_09102024

--- Unidad2_07102024/script.js
+++ Unidad2_07102024/script.js
@@ -3,13 +3,16 @@ function appendToDisplay(value) {
     document.getElementById('display').value += value;
     
     }
-    
+    //con ctrl f puedes buscar una funcion (aqui), por otro lado, en la pag al darle inspeccionar puedes ver el nombre de la funcion
+    //function para definir una funcion y especificamos su nombre de la funcion
+    //aqui las funciones hacen que la calculadora funcione
+    //la function clearDisplay por ejemplo cuando se llama deja un vacio
 function clearDisplay() {
     
     document.getElementById('display').value = '';
     
 }
-    
+    //esta funcion, aqui se permite que al borrar discrimine al ultimo elemento y lo borre al darle click en dell
 function deleteLast() {
 
     let displayValue = document.getElementById('display').value;
@@ -19,7 +22,7 @@ function deleteLast() {
 }
     
 function calculateResult() {
-    
+    //trae el resultado y lo pone en el display, trayendo otra cosa mas
     try {
 
     let result = eval(document.getElementById('display').value);
@@ -27,7 +30,7 @@ function calculateResult() {
     
     } catch (error) {
     
-    alert('Expresión inválida');
+    alert('Expresión inválida'); //control de errores da un mensaje de 'Expresión inválida'
     
     }
     
